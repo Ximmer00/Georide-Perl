a0770587a50b29154c9461ed4d4bfb3acc89543f
Loic VM Boulot <bonnard.loic.07@gmail.com>
2019-04-10 14:52:08 +0200

    Added the pre_install script

---
+++ pre_install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cpan -i JSON::Parse
+cpan -i JSON::MaybeXS
+cpan -i Encode
+cpan -i LWP::UserAgent
+cpan -i HTTP::Request
+cpan -i IO::Prompter
+cpan -i Config::Tiny
+cpan -i Config::Std
+cpan -i DateTime
+cpan -i Date::Calc
+cpan -i Crypt::Lite
+cpan -i URL::Encode
+
+echo "Module installtion done, you can now run the Script"
